ba116d27702ef8ab47d0f8f8edd3a340d55d7787
MateoDeMartino <mateodemartino@gmail.com>
2022-07-03 21:07:39 -0300

    Arreglo error dentro de las urls

--- BlogLeyes/urls.py
+++ BlogLeyes/urls.py
@@ -13,28 +13,28 @@ urlpatterns = [
     #Writer----------------------------------------------------------------------------
     path('writer/list',views.writerList.as_view(),name='writerList'),
     path('writer/<pk>',views.writerDetail.as_view(),name='writerDetail'),
-    path('writer/new',views.writerMake.as_view(),name='writerNew'),
+    path('writer/writerNew/',views.writerMake.as_view(),name='writerNew'),
     path('writer/edit/<pk>',views.writerUpdate.as_view(),name='writerEdit'),
     path('writer/delete/<pk>',views.writerDelete.as_view(),name='writerDelete'),
 
     #Owner----------------------------------------------------------------------------
     path('owner/list',views.ownerList.as_view(), name='ownerList'),
     path('owner/<pk>',views.ownerDetail.as_view(),name='ownerDetail'),
-    path('owner/new',views.ownerMake.as_view(),name='ownerNew'),
+    path('owner/ownerNew/',views.ownerMake.as_view(),name='ownerNew'),
     path('owner/edit/<pk>',views.ownerUpdate.as_view(),name='ownerEdit'),
     path('owner/delete/<pk>',views.ownerDelete.as_view(),name='ownerDelete'),
 
     #Themes----------------------------------------------------------------------------
     path('themes/list', views.themesList.as_view(),name='themesList'),
     path('themes/<pk>', views.themesDetail.as_view(),name='themesDetail'),
-    path('themes/new',views.themesMake.as_view(),name='themesNew'),
+    path('themes/themesNew/',views.themesMake.as_view(),name='themesNew'),
     path('themes/edit/<pk>',views.themesUpdate.as_view(),name='themesEdit'),
     path('themes/delete/<pk>',views.themesDelete.as_view(),name='themesDelete'),
     
     #Donor----------------------------------------------------------------------------
     path('donor/list', views.donorList.as_view(), name='donorList'),
     path('donor/<pk>', views.donorDetail.as_view(),name='donorDetail'),
-    path('donor/new',views.donorMake.as_view(),name='donorNew'),
+    path('donor/donorNew/',views.donorMake.as_view(),name='donorNew'),
     path('donor/edit/<pk>',views.donorUpdate.as_view(),name='donorEdit'),
     path('donor/delete/<pk>',views.donorDelete.as_view(),name='donorDelete'),
 
